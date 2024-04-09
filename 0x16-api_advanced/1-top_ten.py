588fdbe1d71a1ff4bdbd6ec6f682467845e1cfb5
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-04-09 20:09:56 +0200

    alx

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,7 +7,7 @@ import sys
 def top_ten(subreddit):
     """ Returns: top ten post titles
         or None if queried subreddit is invalid """
-    headers = {'User-Agent': 'xica369'}
+    headers = {'User-Agent': 'youssef2766'}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     parameters = {'limit': 10}
     response = requests.get(url, headers=headers, allow_redirects=False,
