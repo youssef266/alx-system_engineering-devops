a15daa751486fd6551cc0e88e908f884534fed6d
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-04-09 12:52:56 +0200

    alx

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,7 @@ def number_of_subscribers(subreddit):
     """queries the Reddit API"""
     headers = {
         "User-Agent": "0x16. API_advanced-e_kiminza"
-        }
+    }
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
