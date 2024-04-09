7dd22bc4d06a4c02d19894123c9a7c82831e952c
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-04-09 20:16:37 +0200

    alx

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """
-contains the number_of_subscribers function
+Contains the number_of_subscribers function
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscriberes for a given subreddit"""
-    if subreddit is None or not isinstance(subreddit, str):
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
         return 0
     r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
                      headers={'User-Agent': 'Python/requests:APIproject:\
-    v1.0.0 (by /u/youssef2766)'}).json()
+v1.0.0 (by /u/youssef2766)'}).json()
     subs = r.get("data", {}).get("subscribers", 0)
     return subs
