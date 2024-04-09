0c96c2dbd86ba105f7bcce2b0ecf9e0e8628f655
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-04-09 19:55:43 +0200

    alx

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
     Returns:
         number of subscribers to the subreddit,
         or 0 if subreddit requested is invalid"""
-    headers = {'User-Agent': 'youssef2766'}
+    headers = {'User-Agent': 'eyJhbGciOiJSUzI1NiIsImtpZCI6IlNIQTI1NjpzS3dsMnlsV0VtMjVmcXhwTU40cWY4MXE2OWFFdWFyMnpLMUdhVGxjdWNZIiwidHlwIjoiSldUIn0.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNzEyNzcxNjc4LjQxMjE3MywiaWF0IjoxNzEyNjg1Mjc4LjQxMjE3MiwianRpIjoiU3hMQ0N3WHNHU1pnVTNsVV9MdmpMQnotVGNKeVR3IiwiY2lkIjoiN2xWWFlJWVpGQzRiVTEtRW04NTRRQSIsImxpZCI6InQyXzVqYmFtZm5zZSIsImFpZCI6InQyXzVqYmFtZm5zZSIsImxjYSI6MTY3NjgzNTA3MTAwMCwic2NwIjoiZUp5S1Z0SlNpZ1VFQUFEX193TnpBU2MiLCJmbG8iOjl9.lXu6bFbr8stG9u1k4GzUAqCeYOCUtMPeOkQBB9mIbT6fCiaVN2SeTKlO-jWQsXY26pjS8k3cMINL6JjsqkV0bkphdKajsG-VZ6M_Jzl9VaXPD8sfS0rz_U81OyqBuU54pfDckJXPBDNCq9KpxFuxaMBFu7rq51cGOnblcp2eYznBz89f6zpwd1VpAxHNWjup_68QQj2k4GMIMAAeQNZ4lp6Q4BQvxRLYlhzFq44wn_8HAaQa2D7eaSHUkuqO1KC7dfm4jFjpY_cvAmqGRvCMONsDzYcPav_YzYdkagavjlPgsm5-eHNzUBn3yZ4FYiELnun2tIOwrjNAycaMMeI45Q'}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
 
