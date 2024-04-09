5b793076dfb365e58e0053376e9a6781b4fbf59d
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-04-09 19:06:56 +0200

    alx

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,7 @@ def number_of_subscribers(subreddit):
         number of subscribers to the subreddit,
         or 0 if subreddit requested is invalid"""
     headers = {'User-Agent': 'xica369'}
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
